191fe5ebc9facb52bc777d5aeba947f876b4f399
sshEdd1e <edson.paschoal141@gmail.com>
2024-01-23 21:46:52 -0300

    Feat: Adicionado mapeamento de Produto

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/mapper/ProdutoMapper.java
@@ -0,0 +1,25 @@
+package com.compassuol.sp.challenge.ecommerce.web.dto.mapper;
+
+import com.compassuol.sp.challenge.ecommerce.entities.Produto;
+import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoCreateDto;
+import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoResponseDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class ProdutoMapper {
+
+    public static Produto toProduto(ProdutoCreateDto createDto){
+        return new ModelMapper().map(createDto, Produto.class);
+    }
+    public static ProdutoResponseDto toDto(Produto produto){
+        return new ModelMapper().map(produto, ProdutoResponseDto.class);
+    }
+
+    public static List<ProdutoResponseDto> toListDto(List<Produto> produtos){
+        return produtos.stream().map(prod -> toDto(prod)).collect(Collectors.toList());
+    }
+}
