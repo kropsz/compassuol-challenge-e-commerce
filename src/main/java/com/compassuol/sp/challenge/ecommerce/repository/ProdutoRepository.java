d1f17719e2de7197cc2ac3dd165bd0d0efd0a2bf
Pedro Esteves <kropinho211@outlook.com>
2024-01-25 12:08:57 -0300

    Feat: Adicionado métodos salvar() e findByName()

--- src/main/java/com/compassuol/sp/challenge/ecommerce/repository/ProdutoRepository.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/repository/ProdutoRepository.java
@@ -3,10 +3,15 @@ package com.compassuol.sp.challenge.ecommerce.repository;
 
 import com.compassuol.sp.challenge.ecommerce.entities.Produto;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    Optional<Produto> findByName(String name);
 
 }
 
