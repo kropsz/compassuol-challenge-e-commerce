1c9afe471de9f76cda9561f110837b326beb3598
Pedro Esteves <kropinho211@outlook.com>
2024-01-25 13:41:36 -0300

    Docs: Adicionado documentação no recurso de criar Produto

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/ProdutoController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/ProdutoController.java
@@ -8,7 +8,12 @@ import com.compassuol.sp.challenge.ecommerce.services.ProdutoService;
 import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoCreateDto;
 import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoResponseDto;
 import com.compassuol.sp.challenge.ecommerce.web.dto.mapper.ProdutoMapper;
+import com.compassuol.sp.challenge.ecommerce.web.exception.ErrorMessage;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +33,18 @@ public class ProdutoController {
     
     private final ProdutoService produtoService;
 
+    @Operation(summary = "Criar um novo produto", description = "Recurso para criar um novo produto",
+            responses = {
+                    @ApiResponse(responseCode = "201",
+                            description = "Recurso criado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoResponseDto.class))),
+                    @ApiResponse(responseCode = "409",
+                            description = "Produto ja registrado no sistema",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422",
+                            description = "Recurso não processado por entrada de dados invalidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping()
     public ResponseEntity<ProdutoResponseDto> create(@RequestBody @Valid ProdutoCreateDto createDto) {
         Produto produto = produtoService.salvar(ProdutoMapper.toProduto(createDto));
