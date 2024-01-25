ae99d8fe75cbdfafc0dd878a3f664028870bd59e
Pedro Esteves <kropinho211@outlook.com>
2024-01-25 12:06:18 -0300

    Feat: Método POST para criar novos Produtos

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/ProdutoController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/ProdutoController.java
@@ -3,30 +3,35 @@ package com.compassuol.sp.challenge.ecommerce.web.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.compassuol.sp.challenge.ecommerce.config.jacoco.ExcludeFromJacocoGeneratedReport;
+import com.compassuol.sp.challenge.ecommerce.entities.Produto;
+import com.compassuol.sp.challenge.ecommerce.services.ProdutoService;
+import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoCreateDto;
+import com.compassuol.sp.challenge.ecommerce.web.dto.ProdutoResponseDto;
+import com.compassuol.sp.challenge.ecommerce.web.dto.mapper.ProdutoMapper;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Tag(name = "Produtos", description = "Contém todas as opereções relativas ao recurso de um produto")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/produtos")
 public class ProdutoController {
     
-    // TODO: Teste simples para o swagger, remover depois
-    @Operation( summary = "Teste",
-                description = "Teste",
-                responses = {
-                    @ApiResponse(
-                        responseCode = "201", description = "Teste"
-                    )
-                })
-    @ExcludeFromJacocoGeneratedReport
-    @GetMapping("/teste-delete-me")
-    public String deleteMeMethod() {
-        return "Delete me";
-    }    
+    private final ProdutoService produtoService;
+
+    @PostMapping()
+    public ResponseEntity<ProdutoResponseDto> create(@RequestBody @Valid ProdutoCreateDto createDto) {
+        Produto produto = produtoService.salvar(ProdutoMapper.toProduto(createDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ProdutoMapper.toDto(produto));
+    }
+    
 }
