5a38394b19f4cceb0ef442f8a9386e88bb94fa3f
Kitricorvus <rn_alopex@protonmail.com>
2024-01-29 19:39:00 -0300

    Feat: Adição de novas classes de erro

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/exception/DataInvalidaException.java
@@ -0,0 +1,7 @@
+package com.compassuol.sp.challenge.ecommerce.exception;
+
+public class DataInvalidaException extends RuntimeException {
+    public DataInvalidaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/exception/StatusInvalidoException.java
@@ -0,0 +1,7 @@
+package com.compassuol.sp.challenge.ecommerce.exception;
+
+public class StatusInvalidoException extends RuntimeException {
+    public StatusInvalidoException(String message) {
+        super(message);
+    }
+}
