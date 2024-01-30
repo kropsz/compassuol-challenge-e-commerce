9b2d59b36c210d440a90d2851a7174af548f3ec5
Pedro Esteves <kropinho211@outlook.com>
2024-01-30 19:21:56 -0300

    Refactor: Alteração no tipo quantidadeProduto na classe PedidoProduto

--- src/main/java/com/compassuol/sp/challenge/ecommerce/entities/Pedido.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/entities/Pedido.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
--- src/main/java/com/compassuol/sp/challenge/ecommerce/repository/PedidoRepository.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/repository/PedidoRepository.java
@@ -4,7 +4,6 @@ import com.compassuol.sp.challenge.ecommerce.entities.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
