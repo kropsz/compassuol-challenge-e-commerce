53fe40362e331725ed460170be385497ea8f7256
sshEdd1e <edson.paschoal141@gmail.com>
2024-01-30 15:38:35 -0300

    Fix: Remoçao do mapeamento e Dto para PedidoProduto

--- src/main/java/com/compassuol/sp/challenge/ecommerce/repository/PedidoProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.compassuol.sp.challenge.ecommerce.repository;
-
-import com.compassuol.sp.challenge.ecommerce.entities.PedidoProduto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoProdutoRepository extends JpaRepository<PedidoProduto, Long> {
-}
