f815419249d9abe9d33f28b513182d3134be8546
Luan <luaneldermendes@gmail.com>
2024-01-30 16:50:04 -0300

    Atualizaão de Repositorio com o status do pedido.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/repository/PedidoRepository.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/repository/PedidoRepository.java
@@ -1,11 +1,15 @@
 package com.compassuol.sp.challenge.ecommerce.repository;
 
 import com.compassuol.sp.challenge.ecommerce.entities.Pedido;
+import com.compassuol.sp.challenge.ecommerce.entities.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByStatus(StatusPedido status);
 }
+
