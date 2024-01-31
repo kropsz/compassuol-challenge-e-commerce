4f35f6424baffeabb216d18199d821fd21786307
Luan <luaneldermendes@gmail.com>
2024-01-31 14:56:53 -0300

    Adaptção de Pedido Service para status.

--- src/main/java/com/compassuol/sp/challenge/ecommerce/services/PedidoService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/services/PedidoService.java
@@ -1,10 +1,12 @@
 package com.compassuol.sp.challenge.ecommerce.services;
 
 import com.compassuol.sp.challenge.ecommerce.entities.Pedido;
+import com.compassuol.sp.challenge.ecommerce.entities.Status;
 import com.compassuol.sp.challenge.ecommerce.exception.CancelamentoInvalidoException;
 import com.compassuol.sp.challenge.ecommerce.exception.PedidoNaoEncontradoException;
 import com.compassuol.sp.challenge.ecommerce.repository.PedidoRepository;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,33 +19,42 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PedidoService {
     private final PedidoRepository pedidoRepository;
+
     @Transactional
-    public Pedido salvar(Pedido orders) {
-        return pedidoRepository.save(orders);
+    public Pedido salvar(Pedido pedido) {
+        return pedidoRepository.save(pedido);
     }
 
     @Transactional
-    public List<Pedido> getAllPedidos(Pedido.Status status) {
-        if (status == null) {
+    public List<Pedido> getAllPedidos(Status status) {
+        if (status != null) {
+            return pedidoRepository.findAllByStatusOrderByCreatedDateDesc(status);
+        } else {
             return pedidoRepository.findAllByOrderByCreatedDateDesc();
         }
-        return pedidoRepository.findAllByStatusOrderByCreatedDateDesc(status);
+    }
+
+    @Transactional
+    public Pedido updatePedido(Long id, Pedido pedidoAtualizado) {
+        Pedido pedidoExistente = pedidoRepository.findById(id)
+                .orElseThrow(() -> new PedidoNaoEncontradoException("Pedido não encontrado"));
+
+        return pedidoRepository.save(pedidoExistente);
     }
 
     @Transactional
     public Pedido cancelarPedido (Long id, String cancelReason) {
         Pedido pedidoParaCancelar = pedidoRepository.findById(id).orElseThrow(() -> new PedidoNaoEncontradoException("O pedido não foi encontrado"));
-        
+
         Duration duration = Duration.between(pedidoParaCancelar.getCreatedDate(), LocalDateTime.now());
         long daysSinceCreation = (duration.toHours() + 23) / 24;
 
-        
         if (daysSinceCreation > 90) {
             throw new CancelamentoInvalidoException("O pedido não pode ser cancelado, data de criação superior à 90 dias");
-        } else if ((pedidoParaCancelar.getStatus().equals(Pedido.Status.SENT))) {
+        } else if ((pedidoParaCancelar.getStatus().equals(com.compassuol.sp.challenge.ecommerce.entities.Status.SENT))) {
             throw new CancelamentoInvalidoException("O pedido não pode ser cancelado, porque já foi enviado");
         } else {
-            pedidoParaCancelar.setStatus(Pedido.Status.CANCELED);
+            pedidoParaCancelar.setStatus(com.compassuol.sp.challenge.ecommerce.entities.Status.CANCELED);
             pedidoParaCancelar.setCancelReason(cancelReason);
             return pedidoRepository.save(pedidoParaCancelar);
         }
