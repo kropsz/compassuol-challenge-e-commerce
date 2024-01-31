e258d04da5e38e547d43bf4867759cbcdeb3f78f
Vendru <brnvendruscolo@gmail.com>
2024-01-31 05:48:13 -0300

    Feat: buscarPorId no PedidoController

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/PedidoController.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/controller/PedidoController.java
@@ -1,6 +1,7 @@
 package com.compassuol.sp.challenge.ecommerce.web.controller;
 
 import com.compassuol.sp.challenge.ecommerce.entities.Pedido;
+import com.compassuol.sp.challenge.ecommerce.exception.PedidoNaoEncontradoException;
 import com.compassuol.sp.challenge.ecommerce.services.PedidoService;
 import com.compassuol.sp.challenge.ecommerce.web.dto.*;
 import com.compassuol.sp.challenge.ecommerce.web.dto.mapper.PedidoMapper;
@@ -28,4 +29,20 @@ public class PedidoController {
         Pedido pedido = pedidoService.salvar(PedidoMapper.toPedido(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(PedidoMapper.toDto(pedido));
     }
+
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pedido> buscarPedidoPorId(@PathVariable Long id) {
+        try {
+            Pedido pedido = pedidoService.buscarPorId(id);
+            return ResponseEntity.ok(pedido);
+        } catch (PedidoNaoEncontradoException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
