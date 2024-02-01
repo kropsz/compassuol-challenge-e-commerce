7ac4b66efe20f56111d580e92feb988b817df947
Kitricorvus <rn_alopex@protonmail.com>
2024-02-01 00:38:28 +0000

    Feat: Criação do DTO para a atualização de um pedido

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/PedidoUpdateDto.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/PedidoUpdateDto.java
@@ -4,6 +4,8 @@ import com.compassuol.sp.challenge.ecommerce.entities.Pedido;
 import com.compassuol.sp.challenge.ecommerce.entities.PedidoProduto;
 import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/mapper/PedidoMapper.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/mapper/PedidoMapper.java
@@ -3,6 +3,8 @@ package com.compassuol.sp.challenge.ecommerce.web.dto.mapper;
 import com.compassuol.sp.challenge.ecommerce.entities.Pedido;
 import com.compassuol.sp.challenge.ecommerce.web.dto.PedidoCreateDto;
 import com.compassuol.sp.challenge.ecommerce.web.dto.PedidoResponseDto;
+import com.compassuol.sp.challenge.ecommerce.web.dto.PedidoUpdateDto;
+
 import org.modelmapper.ModelMapper;
 
 import java.util.List;
@@ -13,10 +15,12 @@ public class PedidoMapper {
     public static Pedido toPedido(PedidoCreateDto createDto) {
         return new ModelMapper().map(createDto, Pedido.class);
     }
+    public static Pedido toPedido(PedidoUpdateDto updateDto) {
+        return new ModelMapper().map(updateDto, Pedido.class);
+    }
     public static PedidoResponseDto toDto(Pedido pedido) {
         return new ModelMapper().map(pedido, PedidoResponseDto.class);
     }
-
     public static List<PedidoResponseDto> toListDto(List<Pedido> pedidos){
         return pedidos.stream().map(orders -> toDto(orders)).collect(Collectors.toList());
     }
