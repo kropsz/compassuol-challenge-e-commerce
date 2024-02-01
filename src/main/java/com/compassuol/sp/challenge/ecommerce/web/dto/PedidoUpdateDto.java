686533783eee4872038db3ae0848be8260bdd602
Pedro Esteves <kropinho211@outlook.com>
2024-02-01 16:43:18 -0300

    Refactor: Remoção de Imports nãoutilizados e métodos duplicados

--- src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/PedidoUpdateDto.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/PedidoUpdateDto.java
@@ -1,12 +1,8 @@
 package com.compassuol.sp.challenge.ecommerce.web.dto;
 
 import com.compassuol.sp.challenge.ecommerce.entities.Pedido;
-import com.compassuol.sp.challenge.ecommerce.entities.PedidoProduto;
 import lombok.*;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class PedidoUpdateDto {
