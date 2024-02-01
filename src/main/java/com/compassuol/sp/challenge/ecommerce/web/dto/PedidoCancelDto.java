ac18582649971dce19a36eb2a444098b035a3a9a
Pedro Esteves <kropinho211@outlook.com>
2024-02-01 16:26:47 -0300

    Feat: Adicionado a classe PedidoCancelDto

---
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/web/dto/PedidoCancelDto.java
@@ -0,0 +1,14 @@
+package com.compassuol.sp.challenge.ecommerce.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class PedidoCancelDto {
+    private String cancelReason;
+}
+
+
