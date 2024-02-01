0717a68643a7d417d1055bc3ec44dbd20b922c39
Kitricorvus <rn_alopex@protonmail.com>
2024-02-01 00:39:12 +0000

    Feat: Desenvolvimento no método para atualizar um pedido

--- src/main/java/com/compassuol/sp/challenge/ecommerce/services/PedidoService.java
+++ src/main/java/com/compassuol/sp/challenge/ecommerce/services/PedidoService.java
@@ -3,16 +3,18 @@ package com.compassuol.sp.challenge.ecommerce.services;
 import com.compassuol.sp.challenge.ecommerce.entities.Pedido;
 import com.compassuol.sp.challenge.ecommerce.exception.CancelamentoInvalidoException;
 import com.compassuol.sp.challenge.ecommerce.exception.PedidoNaoEncontradoException;
+import com.compassuol.sp.challenge.ecommerce.exception.PedidoUpdateErrorException;
 import com.compassuol.sp.challenge.ecommerce.repository.PedidoRepository;
-import com.compassuol.sp.challenge.ecommerce.web.dto.PedidoUpdateDto;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.reflect.Field;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -47,13 +49,47 @@ public class PedidoService {
         } else {
             pedidoParaCancelar.setStatus(Pedido.Status.CANCELED);
             pedidoParaCancelar.setCancelReason(cancelReason);
+            pedidoParaCancelar.setCancelDate(LocalDateTime.now());
             return pedidoRepository.save(pedidoParaCancelar);
         }
     }
     
     @Transactional
-    public Pedido updatePedido(Long id, PedidoUpdateDto PedidoUpdateDto) {
+    public Pedido updatePedido(Long id, Pedido pedidoUpdateData) {
         Pedido pedidoUpdate = pedidoRepository.findById(id).orElseThrow(() -> new PedidoNaoEncontradoException("O pedido não foi encontrado"));
+        
+        if (pedidoUpdateData.getStatus() == Pedido.Status.CANCELED) {
+            throw new PedidoUpdateErrorException("O pedido está cancelado");
+        }
+
+        Field[] fields = Pedido.class.getDeclaredFields();
+
+        boolean hasNonNullField = Arrays.stream(fields)
+                .anyMatch(field -> {
+                    try {
+                        field.setAccessible(true);
+                        return field.get(pedidoUpdateData) != null && !field.get(pedidoUpdateData).toString().trim().isEmpty();
+                    } catch (IllegalAccessException e) {
+                        throw new PedidoUpdateErrorException("Erro ao acessar o campo " + field.getName());
+                    }
+                });
+
+        if (!hasNonNullField) {
+            throw new PedidoUpdateErrorException("Requisição vazia");
+        }
+        
+        Arrays.stream(fields)
+                .forEach(field -> {
+                    try {
+                        field.setAccessible(true);
+                        Object value = field.get(pedidoUpdateData);
+                        if (value != null && !value.toString().trim().isEmpty()) {
+                            field.set(pedidoUpdate, value);
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new PedidoUpdateErrorException("Erro ao acessar o campo " + field.getName());
+                    }
+                });
 
 
         return pedidoRepository.save(pedidoUpdate);
